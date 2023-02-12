f19dd56db6f3f776c56cc420d7120d22831764e8
denismospanov <115163705+denismospanov@users.noreply.github.com>
2023-02-12 14:22:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
 import React from "react";
+import './App.css';
 // TODO: import useFormik from formik library
+import { useFormik} from 'formik';
 
 function App() {
   // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''      
+    },
+    onSubmit: values =>{
+      console.log('form:',values);
+      let arrayValues = Object.keys(values);
+      let lengthValues =arrayValues.length;
+      if (lengthValues === 2) alert(JSON.stringify(`Login Successful!!!`));
+    },
+    validate: values =>{
+      let errors = {};
+      if (!values.email) {
+        errors.email = 'Required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Username should be an email';
+      }
+      if(!values.password) errors.password = 'Required';
+      console.log(errors);
+      return errors;
+    }
+  });
 
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email:</div>
+        <input
+          id="emailField"
+          type="text"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {formik.errors.email ? (
+          <div id="emailError" style={{ color: "red" }}>
+            {formik.errors.email}
+          </div>
+        ) : null}
+        <div>Password:</div>
+        <input
+          id="pswField"
+          type="text"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <br />
+        {formik.errors.password ? (
+          <div id="pswError" style={{ color: "red" }}>
+            {formik.errors.password}
+          </div>
+        ) : null}
+        <button id="submitBtn" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
+
+
